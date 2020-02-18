a7005e1580464f61fefcdeb87ccfda45f6263469
charwrangler404 <charwrangler404@gmail.com>
2020-02-18 15:51:15 -0600

    a lil update

--- get-prices.py
+++ get-prices.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 
-import sqlite3. argparse
+import sqlite3. argparse, scrython
+
+parser = argparse.ArgumentParser('get-prices.py')
+parser.add_argument('cardname', type=str, help='Card name to query database for')
+parser.add_argument('--latest', '-l', type=bool, default=False, help='Return only the latest price', nargs='?')
+parser.add_argument('--average', type=bool, default=False, help='Return the latest avergae price between sets'. nargs='?')
+args = parser.parse_args()
+
+def get_latest(cardname):
+    
